48627f0252f42b7db19133e0f3e1dcba68b2d422
AndrewH2O <AndrewH2O@users.noreply.github.com>
2021-10-20 11:14:16 +0100

    setup example product listing and added useState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+
 function App() {
-  return (
-    <div >
-      <h1>Re-Store</h1>
-    </div>
-  );
+    const [products, setProducts] = useState([
+        {name: 'product1', price: 100.00},
+        {name: 'product2', price: 200.00},
+    ]);
+
+    function addProduct() {
+        // use spread operator copy prevState and add new item
+        // based on previous state name 
+        // product1..n  price=i*100
+        setProducts(prevState => [...prevState, {
+            name: 'product' + (prevState.length + 1), 
+            price: (prevState.length * 100) + 100}])
+    }
+   
+    
+    return (
+        <div>
+            <h1>Re-Store</h1>
+            <ul>
+                {products.map((item, index) => (
+                    <li key={index}>{item.name} - {item.price}</li>
+                ))}
+            </ul>
+            <button onClick={addProduct}>Add product</button>
+        </div>
+    );
 }
 
 export default App;
