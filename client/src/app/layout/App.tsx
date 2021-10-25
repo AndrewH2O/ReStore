298819c071b99928ff1565049a57fe1f13dc3406
AndrewH2O <AndrewH2O@users.noreply.github.com>
2021-10-25 18:59:31 +0100

    Added further material-ui styling

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,40 +1,45 @@
-import { Typography } from '@mui/material';
-import {useEffect, useState } from 'react';
+import { Container, CssBaseline } from '@mui/material';
+import { useEffect, useState } from 'react';
 import Catalog from '../../features/Catalog/Catalog';
 import { Product } from '../models/product';
+import Header from './Header';
 
 function App() {
-    const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
-    useEffect(() => {
-        fetch('https://localhost:5001/api/products')
-            .then(response => response.json())
-            .then(data => setProducts(data))
-    },[]) // 2nd param, an empty array means the useEffect is called only once, 
-    // if there is no 2nd param then useffects run every time a re-render happens
-    
-    function addProduct() {
-        // use spread operator copy prevState and add new item
-        // based on previous state name 
-        // product1..n  price=i*100
-        setProducts(prevState => [...prevState, {
-            id: prevState.length + 101,
-            name: 'product' + (prevState.length + 1),
-            price: (prevState.length * 100) + 100,
-            brand: 'some brand',
-            description: 'some descrpn',
-            pictureUrl: 'http://picsum.photos/200'
-        }])         
-    }
-   
-    
-    return (
-        <>
-            <Typography variant='h1'>Re-Store</Typography>
-            <Catalog products={products} addProduct={addProduct}/>
-            
-        </>
-    );
+  useEffect(() => {
+    fetch('https://localhost:5001/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, []) // 2nd param, an empty array means the useEffect is called only once, 
+  // if there is no 2nd param then useffects run every time a re-render happens
+
+  function addProduct() {
+    // use spread operator copy prevState and add new item
+    // based on previous state name 
+    // product1..n  price=i*100
+    setProducts(prevState => [...prevState, {
+      id: prevState.length + 101,
+      name: 'product' + (prevState.length + 1),
+      price: (prevState.length * 100) + 100,
+      brand: 'some brand',
+      description: 'some descrpn',
+      pictureUrl: 'http://picsum.photos/200'
+    }])
+  }
+
+
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog products={products} addProduct={addProduct} />
+      </Container>
+
+    </>
+
+  );
 }
 
 export default App;
--- client/src/features/Catalog/ProductList.tsx
+++ client/src/features/Catalog/ProductList.tsx
@@ -1,5 +1,5 @@
 
-import { List } from "@mui/material";
+import { Grid } from "@mui/material";
 import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
 
@@ -10,10 +10,12 @@ interface Props {
 export default function ProductList({ products }: Props) {
 
   return (
-    <List>
-      {products.map((product) => 
-        <ProductCard key={product.id} product={product}/>
+    <Grid container spacing={4}>
+      {products.map((product) =>
+        <Grid item xs={4} key={product.id}>
+          <ProductCard  product={product} />
+        </Grid>
       )}
-    </List>
+    </Grid>
   )
 }
