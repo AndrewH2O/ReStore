db31406a7e70a174b59149439f0a42896edb50e3
AndrewH2O <AndrewH2O@users.noreply.github.com>
2021-10-28 17:48:42 +0100

    Added theming along with a toggle switch

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,43 +1,33 @@
-import { Container, CssBaseline } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { useState } from 'react';
 import Catalog from '../../features/Catalog/Catalog';
-import { Product } from '../models/product';
 import Header from './Header';
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(false)
+  const palleteType = darkMode ? 'dark' : 'light'
 
-  useEffect(() => {
-    fetch('https://localhost:5001/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, []) // 2nd param, an empty array means the useEffect is called only once, 
-  // if there is no 2nd param then useffects run every time a re-render happens
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default:palleteType ==='light' ? '#eaeaea':'#121212'
+      }
+    }
+  })
 
-  function addProduct() {
-    // use spread operator copy prevState and add new item
-    // based on previous state name 
-    // product1..n  price=i*100
-    setProducts(prevState => [...prevState, {
-      id: prevState.length + 101,
-      name: 'product' + (prevState.length + 1),
-      price: (prevState.length * 100) + 100,
-      brand: 'some brand',
-      description: 'some descrpn',
-      pictureUrl: 'http://picsum.photos/200'
-    }])
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
   }
 
-
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog />
       </Container>
-
-    </>
+    </ThemeProvider>
 
   );
 }
