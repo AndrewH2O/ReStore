cb1b5ae6f25ac006cb41abd12a65bc93f69f8bd1
AndrewH2O <AndrewH2O@users.noreply.github.com>
2021-11-01 11:45:16 +0000

    Added React Router and additional pages: BrowserRouter added to Index, and Route added to App additional pages Contact, Home, About, ProductDetails

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import './app/layout/styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
