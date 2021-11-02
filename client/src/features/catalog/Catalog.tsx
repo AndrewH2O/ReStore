085e0945bc4b7c77462641a1988c69811f5941c7
AndrewH2O <AndrewH2O@users.noreply.github.com>
2021-11-02 17:42:28 +0000

    Centralise Axios code and replace fetch code in Catalog

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,15 +1,15 @@
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 import {useEffect, useState} from "react";
+import agent from "../../app/api/agent";
 
 // instead of props destructure Props into {products, addProduct}
 export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
-        fetch('https://localhost:5001/api/products')
-            .then(response => response.json())
-            .then(data => setProducts(data))
+        // we have centralised axios withinapp api agent
+        agent.Catalog.list().then(products => setProducts(products))
     }, []) // 2nd param, an empty array means the useEffect is called only once, 
     // if there is no 2nd param then useffects run every time a re-render happens
 
