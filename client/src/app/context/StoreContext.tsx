320ad8df1d61616a2ec70a3c244513877f0de841
AndrewH2O <AndrewH2O@users.noreply.github.com>
2021-11-08 12:50:44 +0000

    Added a store context that wraps the app This uses a custom hook to make available a StoreContext for use with the basket

--- client/src/app/context/StoreContext.tsx
+++ client/src/app/context/StoreContext.tsx
@@ -1,12 +1,55 @@
-import { createContext } from "react";
+import { createContext, PropsWithChildren, useContext, useState } from "react";
 import { Basket } from "../models/basket";
 
 // we don't need an addItem as whenever an item is added to the basket
-// we get the basket via setBasket
+// as we get the basket via setBasket
 interface StoreContextValue {
-    basket: Basket|null;
+    basket: Basket|null;     // | can be null
     setBasket:(basket: Basket) => void;
     removeItem: (productId: number, quantity: number) => void;
 }
 
+// default value is undefined
 export const StoreContext = createContext<StoreContextValue | undefined>(undefined);
+
+// custom react hook to work with storeContext
+export function useStoreContext() {
+    const context = useContext(StoreContext);
+    
+    if(context == undefined){
+        throw Error('oops we do not seem to be inside the provider');
+    }
+    
+    return context;
+}
+
+export function StoreProvider({children}:PropsWithChildren<any>) {
+    // create store and methods
+    const[basket,setBasket] = useState<Basket|null>(null);
+    
+    function removeItem(productId: number, quantity: number){
+        if(!basket) return;
+        
+        // copy state into items using the spread operator
+        // React way is to not mutate state
+        const items = [...basket.items];
+        
+        // return -1 if no item found
+        const itemIndex = items.findIndex(i=>i.productId === productId);
+        
+        // on removal either reduce the qty or reduce qty and remove basket item
+        if(itemIndex >= 0) {
+            items[itemIndex].quantity -= quantity;
+            if(items[itemIndex].quantity === 0)items.splice(itemIndex,1)
+            setBasket(prevState => {
+                return {...prevState!, items} //! overrides type safety possible error warning on prevState
+            })
+        }
+    }
+    
+    return (
+        <StoreContext.Provider value={{basket, setBasket, removeItem}}>
+            {children}
+        </StoreContext.Provider>
+    )
+}
