00d110a414dc2a8c284eb4daf46057cb1adfabd4
AndrewH2O <AndrewH2O@users.noreply.github.com>
2021-11-18 15:26:44 +0000

    React redux actions as defined in the Reducer and used in the ContactPage.tsx

--- client/src/features/contact/counterReducer.tsx
+++ client/src/features/contact/counterReducer.tsx
@@ -1,4 +1,9 @@
 // basic example
+
+// actions
+export const INCREMENT_COUNTER = "INCREMENT_COUNTER";
+export const DECREMENT_COUNTER = "DECREMENT_COUNTER";
+
 export interface CounterState {
     data : number;
     title: string;
@@ -10,6 +15,30 @@ const initialState:CounterState = {
 }
 
 // actions are dispatched to reducers to change their state
+// these are the actions and how they create state changes from copying previous state 
+// then updating the properties that change
 export default function counterReducer(state = initialState, action: any){
+    switch (action.type) {
+        case INCREMENT_COUNTER:
+            return {
+                // cannot mutate state e.g. state.data + 1 
+                // instead create a new copy
+                // use spread operator to copy the override data with new value
+                ...state,
+                data: state.data + 1
+            }
+            break;
+        case DECREMENT_COUNTER:
+            return {
+                ...state,
+                data: state.data - 1
+            }
+            break;
+        default:
+            return state; // always return state
+    }
+    
+   
+   
     return state;
 }
