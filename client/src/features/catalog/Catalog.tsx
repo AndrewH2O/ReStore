6e0cd627c3df0ceb22e178137691b17c4aedab6c
AndrewH2O <AndrewH2O@users.noreply.github.com>
2021-11-30 18:05:04 +0000

    Getting filter lists from API and storing in redux

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -2,21 +2,27 @@ import ProductList from "./ProductList";
 import {useEffect} from "react";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import {useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import {fetchProductsAsync, productSelectors } from "./catalogSlice";
+import {fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
 
 // instead of props destructure Props into {products, addProduct}
 export default function Catalog() {
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status} = useAppSelector(state => state.catalog);
+    const {productsLoaded, status, filtersLoaded} = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
     
     
     useEffect(() => {
         if(!productsLoaded) dispatch(fetchProductsAsync())
-    }, [productsLoaded, dispatch]) 
+    }, [dispatch, productsLoaded]) 
     // 2nd param, an empty array means the useEffect is called only once, 
     // if there is no 2nd param then useffects run every time a re-render happens
 
+    // need another useEffect as once productsLoads then if filters is pending then useEffect will run again
+    // we get a duplication need to avoid that
+    useEffect(()=>{
+        if(!filtersLoaded) dispatch(fetchFilters())
+    },[dispatch, filtersLoaded])
+    
     if(status.includes('pending')) return <LoadingComponent message='loading products...'/>
     
     return (//Fragment shorthand <> 
